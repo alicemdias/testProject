a489882770ee15ecb48720089cb53455aa5829a1
alicemdias <alicemdias@poli.ufrj.br>
2024-08-30 17:46:13 +0100

    border

--- src/components/input.vue
+++ src/components/input.vue
@@ -48,7 +48,7 @@ export default Vue.extend({
 </script>
 <template>
   <!-- Create a container so the background can be dark-->
-  <b-container class="border p-3 rounded bg-dark text-white shadow-lg mb-4">
+  <b-container>
     <div>
       <!-- Form group label that displays the text that will be shown as the label.-->
       <b-form-group :for="name">{{ label }}</b-form-group>
